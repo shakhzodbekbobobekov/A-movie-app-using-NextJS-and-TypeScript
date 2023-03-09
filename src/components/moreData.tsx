a9a3c891fee57dec2e2e3a61231fae25edaeafe6
Acer <shaxzodbekbobobekov@gmail.com>
2023-03-09 20:36:24 +0500

    new pages were created and functions were written inside these pages

---
+++ src/components/moreData.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function MoreData() {
+  return <div>MoreData</div>;
+}
+
+export default MoreData;
---
+++ src/pages/account.tsx
@@ -0,0 +1,17 @@
+import Head from "next/head";
+import React from "react";
+
+function account() {
+  return (
+    <>
+      <Head>
+        <title>Your Account</title>
+        <meta name="description" content="Configure your account" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </>
+  );
+}
+
+export default account;
