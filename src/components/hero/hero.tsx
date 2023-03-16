a6f88631f72df9ca27468cb4653bcf3030c1fc91
shakhzodbekbobobekov <shaxzodbekbobobekov@gmail.com>
2023-03-16 18:05:38 +0500

    new Added functions and stylesw

--- src/components/hero/hero.tsx
+++ src/components/hero/hero.tsx
@@ -3,7 +3,8 @@ import { HeroProps } from "./hero.props";
 import { IMovie } from "../../interface/app.interface";
 import Image from "next/image";
 import { image_base } from "src/helpers/constants";
-import {BsPlay} from "react-icons/bs"
+import { BsPlay } from "react-icons/bs";
+import ReactStars from "react-stars";
 
 const Hero = ({ trending }: HeroProps): JSX.Element => {
   const [movie, setMovie] = useState<IMovie>({} as IMovie);
@@ -15,7 +16,7 @@ const Hero = ({ trending }: HeroProps): JSX.Element => {
   }, [trending]);
 
   return (
-    <div className="flex flex-col space-y-2 py-20 mp:space-y-4 lg:h-[65vh] lg:pb-12 lg:justify-end  ">
+    <div className="flex flex-col space-y-2 py-20 mp:space-y-4 lg:h-[65vh] lg:pb-12 lg:center  ">
       <div className="absolute top-0 left-0 -z-10 h-[95vh] w-full">
         <Image
           src={`${image_base}${movie?.backdrop_path || movie?.poster_path}`}
@@ -25,19 +26,36 @@ const Hero = ({ trending }: HeroProps): JSX.Element => {
         />
       </div>
 
+      <div className="py-[4px] px-[8px] rounded-bl-[8px] rounded-tr-[8px] text-center bg-[#1d1d1d]/50 inline-block w-[100px]  ">
+        {movie.media_type}
+      </div>
+
+      <div className="flex items-center space-x-2">
+        <ReactStars
+          edit={false}
+          count={10}
+          value={movie.vote_average}
+          color2={"#fff"}
+        />
+        <p>({movie.vote_count})</p>
+      </div>
+
       <h1 className="text-2xl font-bold md:text-4xl lg:text:-7xl">
         {movie?.title || movie?.name}
       </h1>
       <p className="max-w-xs md:max-w-lg lg:max-w-2xl text-xs text-shadow-md md:text-lg lg:text-2xl">
-        {movie.overview}
+        {movie?.overview?.slice(0, 150)}...
       </p>
       <div>
         <button className=" flex justify-center items-center space-x-2 bg-transparent border-solid border-2 w-[200px] h-[56px]   rounded-full ">
-          <BsPlay className="h-5 w-5 md:h-8 md:w-8"/> Watch now
+          <BsPlay className="h-5 w-5 md:h-8 md:w-8" /> Watch now
         </button>
+        
       </div>
     </div>
   );
 };
 
 export default Hero;
+
+
